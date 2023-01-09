71d5038a27bc3f9dde1c4b062fa7926f9edca809
Evan Lin <linevan6@gmail.com>
2023-01-09 13:57:38 -0500

    Add WIP speed calculation method

--- Assets/CarPort/Scripts/GameplayManager.cs
+++ Assets/CarPort/Scripts/GameplayManager.cs
@@ -7,11 +7,13 @@ public class GameplayManager : MonoBehaviour
 {
     public Text scoreText;
     private int currentScore;
+
     public Text laneText;
     private int currentScore2;
-    public Text speedText;
-    private int currentSpeed;
 
+    public Text speedText;
+    private float currentSpeed;
+    public float UpdateDelay = 5;
 
     public void UpdateScore(int score)
     {
@@ -24,10 +26,41 @@ public class GameplayManager : MonoBehaviour
         currentScore2 += score;
         laneText.text = "Crosses: " + currentScore2.ToString();
     }
-    public void UpdateSpeed(int speed) {
+    public void UpdateSpeed(float speed) {
         //currentSpeed = (Rigidbody X Axis Speed + Z Axis Speed)/2 == Average speed across X and Z Axis?
         //speedText.text = "Speed: " + currentSpeed.ToString();
     }
+
+    /*void OnEnabled()
+    {
+        StartCoroutine(SpeedCalculation()); //starts speed calculation routine
+    }
+
+    private IEnumerator SpeedCalculation()
+    {
+
+        YieldInstruction timedWait = new WaitForSeconds(UpdateDelay); //creates a yield instruction that pauses the function for UpdateDelay time
+        Vector3 lastPosition = transform.position; //current position
+        float lastTimestamp = Time.time; //the time at the beginning of the current frame
+
+        while (enabled)
+        {
+            yield return timedWait; 
+
+            var deltaPosition = (transform.position - lastPosition).magnitude; //grabs the distance between the two positions
+            var deltaTime = Time.time - lastTimestamp; //var --> Variables whose types are dependent on its usage
+
+            if (Mathf.Approximately(deltaPosition, 0f)) // Clean up "near-zero" displacement
+                deltaPosition = 0f;
+
+            currentSpeed = deltaPosition / deltaTime; //actual speed calculation
+
+            lastPosition = transform.position; //resets position
+            lastTimestamp = Time.time; //resets time
+        }
+        UpdateSpeed(currentSpeed); //might need to change position of method
+    }*/
+
 
     // Start is called before the first frame update
     void Start() 
