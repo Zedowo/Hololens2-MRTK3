b3dc2f850954abfcccf3c267c3b1eb0ec05931ad
noah <209639@pemb.org>
2023-01-11 09:59:40 -0500

    Began on left turn signal script

--- Assets/UserTest/Scripts/LeftTurnSignal.cs
+++ Assets/UserTest/Scripts/LeftTurnSignal.cs
@@ -2,22 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class LeftTurnSignal : MonoBehaviour
 {
     private GameplayManager gamePlayManager;
-    private int value = 0;
+    //private int value = 0;
+
+    public GameObject leftTurnText;
+    public GameObject leftTurnStick;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Green Colors is 95 210 20 255, Hex is 5FD214
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.rotation.z < -90)
-        {
+        if (leftTurnStick.transform.rotation.z >= -30) { //rotation of turn signal at which it is considered "On"
+            leftTurnText.SetActive(true);
             Debug.Log("Turn!");
+        }
+        else {
+            leftTurnText.SetActive(false);
         }
     }
 }
