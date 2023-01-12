82125cb2499c8565b621190b715989b3ec57a6fb
noah <209639@pemb.org>
2023-01-12 12:09:32 -0500

    Broke ahhh

--- Assets/UserTest/Scripts/LeftTurnSignal.cs
+++ Assets/UserTest/Scripts/LeftTurnSignal.cs
@@ -12,6 +12,17 @@ public class LeftTurnSignal : MonoBehaviour
     //public GameObject leftTurnStick = GameObject.FindWithTag("TurnSignal");
 
     // Start is called before the first frame update
+    
+    //IEnumerator Blink()
+    //{
+     //   Debug.Log("Blinker On");
+
+//        gamePlayManager.updateLeftSignal(1);
+  //      yield return new WaitForSeconds(0.5f);
+    //    gamePlayManager.updateLeftSignal(0);
+
+//    }
+
     void Start()
     {
         //Green Colors is 95 210 20 255, Hex is 5FD214
@@ -21,10 +32,19 @@ public class LeftTurnSignal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //while(transform.rotation.z >= 0)
+        //{
+            //StartCoroutine(Blink());
+        //}
+        //gamePlayManager.updateLeftSignal(0);
+
+
+
         if (transform.rotation.z >= 0) { //rotation of turn signal at which it is considered "On"
             //leftTurnText.SetActive(true);
             Debug.Log("Turn!");
             gamePlayManager.updateLeftSignal(1);
+            //Invoke("gamePlayManager.updateLeftSignal(0)", 0.5f);
         }
         else {
             //leftTurnText.SetActive(false);
